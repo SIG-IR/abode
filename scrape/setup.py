6e0dc363762afb68c5e9f75fff321ef18abd6902
Nathan <34ndju@gmail.com>
2017-09-29 00:02:55 -0500

    added setup.py and fixed .gitignore

---
+++ scrape/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='api',
+      version='0.0.0',
+      packages=find_packages(),
+      install_requires=[
+        'BeautifulSoup'
+      ],
+)
