f43ef538e4ad1816ec1f284d084b23ae66c7f54e
Nathan <34ndju@gmail.com>
2017-10-02 00:42:05 -0500

    Support for landlord, renamed amenities to info, and pics

--- scrape/marshall_scrape_test.py
+++ scrape/marshall_scrape_test.py
@@ -2,14 +2,16 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def find_num_beds(string):
-    if string.find('One bed') > -1:
+    if string.find('One bed') > -1 or string.find('1 bed') > -1:
         return 1
-    elif string.find('Two bed') > -1:
+    elif string.find('Two bed') > -1 or string.find('2 bed') > -1:
             return 2
-    elif string.find('Three bed') > -1:
+    elif string.find('Three bed') > -1 or string.find('3 bed') > -1:
             return 3
-    elif string.find('Four bed') > -1:
+    elif string.find('Four bed') > -1 or string.find('4 bed') > -1:
         return 4
+    elif string.find('Five bed') > -1 or string.find('5 bed') > -1:
+        return 5
     else:
         return -1
 
@@ -17,23 +19,23 @@ def find_price(string):
     index_of_dollar = string.find('$')
     index_of_next_space = string.find(' ', index_of_dollar)
     if index_of_dollar > -1 and index_of_next_space > -1:
-        return string[index_of_dollar + 1: index_of_next_space]
+        return string[index_of_dollar + 1: index_of_next_space].replace(',', '')
     else:
         return -1
 
-def look_for_and_remove_price(amenities):
+def look_for_and_remove_price(info):
     prices = []
     price = {}
-    for a in amenities:
-        if find_num_beds(a) > -1 and a.find('$') > -1:
-            price['num_beds'] = find_num_beds(a)
-            price['monthly_rate'] = int(find_price(a))
+    for i in info:
+        if find_num_beds(i) > -1 and i.find('$') > -1:
+            price['num_beds'] = find_num_beds(i)
+            price['monthly_rate'] = int(find_price(i))
             prices.append(price)
             price = {}
-        elif a.find('$') > -1:
-            price['monthly_rate'] = int(find_price(a))
-        elif find_num_beds(a) > -1:
-            price['num_beds'] = find_num_beds(a)
+        elif i.find('$') > -1:
+            price['monthly_rate'] = int(find_price(i))
+        elif find_num_beds(i) > -1:
+            price['num_beds'] = find_num_beds(i)
 
         if 'num_beds' in price and 'monthly_rate' in price:
             prices.append(price)
@@ -41,37 +43,52 @@ def look_for_and_remove_price(amenities):
 
     return prices
 
+def find_image_links(soup):
+    img_links = []
+    for img in soup.find_all('img'):
+        img_links.append("http://champaignmarshallapartments.com/"  + img['src'])
+    return img_links
+
 
 def create_apartment_from_soup(soup, link):
     apartment_schema = {}
     apartment_schema['link'] = link
     apartment_schema['address'] = soup.find('h1', {'class', 'heading1'}).string
-    apartment_schema['rate'] = look_for_and_remove_price(amenities)
+    apartment_schema['rate'] = look_for_and_remove_price(info)
+    apartment_schema['pictures'] = find_image_links(soup)
+    apartment_schema['landlord'] = {
+            'email': None,
+            'name': None,
+            'phone': '217-356-1407'
+    }
 
     print apartment_schema
 
 
 
+apt_link = 'http://champaignmarshallapartments.com/105_E_John.html'
 
-html = urllib2.urlopen('http://champaignmarshallapartments.com/105_E_John.html').read()
+html = urllib2.urlopen(apt_link).read()
 
 soup = BeautifulSoup(html, 'lxml')
 
 lis = soup.find('ul', {'class', 'position2'}).find_all('li')
 
-amenities = []
+info = []
 visited_links = []
 
 for li in lis:
-    amenities.append(li.string)
+    info.append(li.string)
 
 #for link in soup.findAll('a', href=True):
     #print "http://champaignmarshallapartments.com/" + link['href']
 
-for a in amenities:
+for a in info:
     print a
 
-create_apartment_from_soup(soup, 'http://champaignmarshallapartments.com/110_E_John.html')
+find_image_links(soup)
+
+create_apartment_from_soup(soup, apt_link)
 
 
 
@@ -87,6 +104,7 @@ apartment schema: { link: String,
                                 name: String,
                                 phone: String },
                     pictures: String[],
-                    amenities: String[]
+                    info: String[],
+                    pets: Boolean
                 }
 '''
