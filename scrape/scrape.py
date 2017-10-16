af5942efc8ed9907749ebc09319d1d4d73bb937f
shreychowdhary <shrey.chowdhary1@gmail.com>
2017-10-15 21:39:19 -0500

    setup bed and bathroom scraping

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -3,43 +3,58 @@ from selenium import webdriver
 import re
 import json
 
+
 def scrape(json_file):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    browser = webdriver.Chrome(executable_path="./chromedriver",chrome_options=options)
+    browser = webdriver.Chrome(
+        executable_path="./chromedriver", chrome_options=options)
 
     raw_json = open(json_file, "r").read()
-    config = json.loads(raw_json);
+    config = json.loads(raw_json)
 
     browser.get(config["link"])
-    soup = BeautifulSoup(browser.page_source,"html5lib")
+    soup = BeautifulSoup(browser.page_source, "html5lib")
     main = soup.main
 
     output = {}
     if "beds" in config:
         output["beds"] = config["beds"]
     else:
-        pass
-        #bed scrapping here
+        #find full string
+        beds_full_str = soup.find(recursive=True, text=re.compile("\d (bed)",re.I))
+        #find bed part
+        beds_str = re.search("\d (bed)", beds_full_str, re.I).group(0)
+        #find Number
+        beds = int(re.search("\d", beds_str).group(0))
+        output["beds"] = beds
+        # bed scrapping here
+
 
     if "bathrooms" in config:
         output["bathrooms"] = config["bathrooms"]
     else:
-        pass
-        #bed scrapping here
+        #find full string
+        bathroom_full_str = soup.find(recursive=True, text=re.compile("(\d (bath))",re.I))
+        #find bathroom part
+        bathroom_str = re.search("(\d (bath))", bathroom_full_str, re.I).group(0)
+        #find Number
+        bathroom = int(re.search("\d", bathroom_str).group(0))
+        output["bathrooms"] = bathroom
+        # bed scrapping here
 
-    #get all images
+    # get all images
     images = []
     for img in main.find_all("img"):
         images.append(img["src"])
     output["images"] = images
 
-    #find price
+    # find price
     price_pattern = re.compile("\$\d{3,4}")
-    price_str = soup.find_all(text=price_pattern,recursive=True)
+    price_str = soup.find_all(text=price_pattern, recursive=True)
 
-    price = int(re.search("\d+",price_str[0]).group())
-    print price
+    price = int(re.search("\d+", price_str[0]).group())
+
     if config["rate"] == "per-person":
         output["price"] = price * output["beds"]
     else:
@@ -48,11 +63,15 @@ def scrape(json_file):
     if config["info-given"]:
         output["info"] = config["info"]
     else:
-        output["info"] = main.find_all(config["info"],recursive=True)
+        info_list = []
+        for info in main.find_all(config["info"], recursive=True):
+            info_list.append(info.contents)
+        output["info"] = info_list
 
     print output
 
     # TODO: address, phone,beds,bathroom
+
 
 if __name__ == "__main__":
     scrape("./json/309_2bed.json")
